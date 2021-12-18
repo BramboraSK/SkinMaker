b7e2345b745854e9bcedd9565d2ae6f7c9a92dbb
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2021-12-18 21:57:56 +0100

    Windows and MainWindow edit
    Added windows so we can work on them separately + edited MainWindow.

--- SkinMaker/EditorWindow.xaml.cs
+++ SkinMaker/EditorWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SkinMaker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interakční logika pro EditorWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EditorWindow : Window
     {
-        public MainWindow()
+        public EditorWindow()
         {
             InitializeComponent();
-        }
-
-        private void NewSkin_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- SkinMaker/OptionsWindow.xaml.cs
+++ SkinMaker/OptionsWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SkinMaker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interakční logika pro OptionsWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OptionsWindow : Window
     {
-        public MainWindow()
+        public OptionsWindow()
         {
             InitializeComponent();
-        }
-
-        private void NewSkin_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
