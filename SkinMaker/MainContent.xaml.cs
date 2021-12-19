8a88227b26f5476cd8065c19b1a20ba189b19c74
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2021-12-19 01:59:57 +0100

    Bunch of stuff
    Added a bunch of important stuff #1

---
+++ SkinMaker/MainContent.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkinMaker
+{
+    /// <summary>
+    /// Interakční logika pro MainContent.xaml
+    /// </summary>
+    public partial class MainContent : UserControl
+    {
+        public MainContent()
+        {
+            InitializeComponent();
+        }
+        private void NewSkin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Options_Click(object sender, RoutedEventArgs e)
+        {
+            this.Content = new OptionsContent();
+        }
+
+        private void Shutdown_MouseDown(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Minimize_MouseDown(object sender, RoutedEventArgs e)
+        {
+            MainWindow.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+    }
+}
