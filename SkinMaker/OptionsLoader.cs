a4b4508c5d7a738429985b0687c304247819bda5
BramboraSK <majojo992@gmail.com>
2021-12-19 19:59:41 +0100

    jak jede ne
    xdd

---
+++ SkinMaker/OptionsLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace SkinMaker
+{
+    public class OptionsLoader
+    {
+        public class OptionsFile
+        {
+            public string SkinsFolderPath { get; set; }
+        }
+
+        private static readonly string optionsFilename = "options.json";
+        public static OptionsFile options;
+
+        public void Init()
+        {            
+            if (!File.Exists(optionsFilename))
+            {
+                string dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"osu!\Skins");
+
+                OptionsFile newFile = new()
+                {
+                    SkinsFolderPath = Directory.Exists(dir) ? dir : ""
+                };
+
+                File.WriteAllText(optionsFilename, JsonSerializer.Serialize(newFile));
+            }
+
+            options = JsonSerializer.Deserialize<OptionsFile>(File.ReadAllText(optionsFilename));
+        }
+
+        public static void Save()
+        {
+            File.WriteAllText(optionsFilename, JsonSerializer.Serialize(options));
+        }
+    }
+}
